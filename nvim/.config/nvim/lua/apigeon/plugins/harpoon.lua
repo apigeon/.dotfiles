ad1e8e90e1b5827d29beee2b27872b7084bcff5e
Alexandre Pigeon <alexandre.pigeon@gmail.com>
2025-11-01 18:49:51 +0100

    update nvim config

---
+++ nvim/.config/nvim/lua/apigeon/plugins/harpoon.lua
@@ -0,0 +1,52 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+
+		harpoon:setup()
+
+		local set = vim.keymap.set
+
+		set("n", "<leader>q", function()
+			harpoon:list():add() -- add file to harpoon
+		end)
+		set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list()) -- toggle menu
+		end)
+
+		-- Switch to harpooned file 1-4
+		set("n", "<leader>a", function()
+			harpoon:list():select(1)
+		end)
+		set("n", "<leader>z", function()
+			harpoon:list():select(2)
+		end)
+		set("n", "<leader>e", function()
+			harpoon:list():select(3)
+		end)
+		set("n", "<leader>r", function()
+			harpoon:list():select(4)
+		end)
+
+		-- susbstitute harpoon file 1-4 with new files
+		set("n", "<leader><C-a>", function()
+			harpoon:list():replace_at(1)
+		end)
+		set("n", "<leader><C-z>", function()
+			harpoon:list():replace_at(2)
+		end)
+		set("n", "<leader><C-e>", function()
+			harpoon:list():replace_at(3)
+		end)
+		set("n", "<leader><C-r>", function()
+			harpoon:list():replace_at(4)
+		end)
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		-- set("n", "<C-S-P>", function() harpoon:list():prev() end)
+		-- set("n", "<C-S-N>", function() harpoon:list():next() end)
+	end,
+}
---
+++ nvim/.config/nvim/lua/apigeon/plugins/lazygit.lua
@@ -0,0 +1,14 @@
+return {
+	-- "kdheepak/lazygit.nvim",
+	-- lazy = false,
+	-- cmd = {
+	--     "LazyGit",
+	--     "LazyGitConfig",
+	--     "LazyGitCurrentFile",
+	--     "LazyGitFilter",
+	--     "LazyGitFilterCurrentFile",
+	-- },
+	-- keys = {
+	--     { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+	-- },
+}
